25ecfd42c5dd33b99b451080ab9e992822f46773
m_emin <mehmeteminbul@gmail.com>
2021-12-11 18:43:26 +0300

    Letters Count task added

---
+++ Letters Count.py
@@ -0,0 +1,12 @@
+
+#task Letters Count
+sentence = input("type sentece: ")
+sentence = list(sentence)
+empty_dict = {}
+for i in sentence:
+    empty_dict [i] = sentence.count(i)
+print(empty_dict)
+
+# https://github.com/eminbulbul/phyton_assigments
+
+
